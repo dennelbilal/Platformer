baaf9bc9cde27e59d840d06256f23daa5c7d0c49
dennelbilal <72392273+dennelbilal@users.noreply.github.com>
2021-03-07 16:20:46 +0100

    Add files via upload

--- src/scenes/Zoo.js
+++ src/scenes/Zoo.js
@@ -8,6 +8,7 @@ class Zoo extends Tableau{
     }
     create() {
         super.create();
+        
         //quelques étoiles
         let largeur=64*2;
         this.stars=this.physics.add.group();
@@ -30,6 +31,29 @@ class Zoo extends Tableau{
         this.monstre.setBounce(1);
         this.monstre.setVelocityX(50);
         this.physics.add.overlap(this.player, this.monstre, this.hitSpike, null, this);
+
+            //on définit la taille du tableau
+            let largeurDuTableau=4000;
+            let hauteurDuTableau=600; //la hauteur est identique au cadre du jeu
+            this.cameras.main.setBounds(0, 0, largeurDuTableau, hauteurDuTableau);
+            this.physics.world.setBounds(0, 0, largeurDuTableau,  hauteurDuTableau);
+    
+            this.cameras.main.startFollow(this.player, false, 0.05, 0.05);
+    
+            //quelques étoiles et plateformes qui vont avec
+            this.stars=this.physics.add.group();
+            this.platforms=this.physics.add.staticGroup();
+            for(let posX=20;posX<largeurDuTableau;posX+=100){
+                let etoileY=350+Math.sin(posX)*100;
+                let star=this.stars.create(posX ,etoileY,"star");
+                star.body.allowGravity=false;
+                let plate=this.platforms.create(posX ,etoileY+50,"ground");
+                plate.setDisplaySize(60,10);
+                plate.refreshBody();
+            }
+            this.physics.add.overlap(this.player, this.stars, this.ramasserEtoile, null, this);
+            this.physics.add.collider(this.player,this.platforms);
+    
 
         //on change de ciel, on fait une tileSprite ce qui permet d'avoir une image qui se répète
         this.sky=this.add.tileSprite(
